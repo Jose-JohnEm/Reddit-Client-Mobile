dd29bad36f77187751d6d85a49e5a38201cd4e5a
Jonathan José <jonathan-emmanuel.jose@epitech.eu>
2021-10-31 16:29:05 +0100

    Now we can read video

--- SubPost.tsx
+++ SubPost.tsx
@@ -1,6 +1,7 @@
 import { Card, WhiteSpace, WingBlank } from "@ant-design/react-native"
 import React, { useState } from "react"
-import { StyleSheet, Text, View, Image } from "react-native"
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native"
+import VideoPlayer from "react-native-video-player"
 import fetchOAuth from "./Fetchoauth"
 
 export type SPprops = {
@@ -26,12 +27,62 @@ const styles = StyleSheet.create({
 
 const SubPost : React.FC<SPprops> = ({ parsed }) => {
     const [subredditPic, setSubredditPic] = useState('https://www.elementaryos-fr.org/wp-content/uploads/2019/08/logo-reddit.png')
+    const [postPic, setPostPic] = useState({
+        uri: 'https://www.elementaryos-fr.org/wp-content/uploads/2019/08/logo-reddit.png',
+        width: 10,
+        height: 10,
+    })
     const getSubredditPic = () => {
         fetchOAuth(`https://oauth.reddit.com/r/${parsed.data.subreddit}/about/`)
         .then((response) => response.json())
         .then(json => {
-            setSubredditPic(json.data.icon_img)
-        })
+                setSubredditPic(json.data.icon_img)
+                if (parsed.data.preview && parsed.data.preview.images && !parsed.data.is_video) {
+
+                    setPostPic({
+                        uri: parsed.data.url,
+                        width: parsed.data.preview.images[0].resolutions[2].width,
+                        height: parsed.data.preview.images[0].resolutions[2].height
+                    })
+                } else if (parsed.data.is_video) {
+                    setPostPic({
+                        uri: parsed.data.media.reddit_video.fallback_url,
+                        width: parsed.data.media.reddit_video.width,
+                        height: parsed.data.media.reddit_video.height
+                    })
+                }
+            }
+        )
+    }
+
+    const picOrVid = () => {
+        if (parsed.data.is_video) {
+            // return <VideoPlayer
+            //     videoWidth={100}
+            //     videoHeight={100}
+            //     video={{uri: 'https://static.videezy.com/system/resources/previews/000/000/248/original/http-bing.mp4'}}
+            //     autoplay={true}
+            //     defaultMuted={true}
+                // thumbnail={{uri: 'https://www.elementaryos-fr.org/wp-content/uploads/2019/08/logo-reddit.png'}}
+            //     />
+            return (
+                <ScrollView scrollEnabled={false} nestedScrollEnabled={false}>
+                    <Text>ça marche pas lol</Text>
+                    <VideoPlayer
+                        videoWidth={postPic.width}
+                        videoHeight={postPic.height}
+                        video={{uri: postPic.uri}}
+                        autoplay={false}
+                        defaultMuted={true}
+                        loop={true}
+                        pauseOnPress={true}
+                        hideControlsOnStart={true}
+                    />
+                </ScrollView>
+            )
+        } else {
+            return <Image style={{width: postPic.width, height: postPic.height, marginLeft:27}} source={{ uri: postPic.uri}}/>
+        }
     }
 
     getSubredditPic()
@@ -39,6 +90,7 @@ const SubPost : React.FC<SPprops> = ({ parsed }) => {
         <View>
             <WhiteSpace />
             <WhiteSpace />
+
             <WingBlank size="lg">
                 <Card style={styles.bg}>
                     <Card.Header
@@ -49,8 +101,8 @@ const SubPost : React.FC<SPprops> = ({ parsed }) => {
                     <Card.Body>
                         <View>
                             <Text style={styles.title}>{parsed.data.title}</Text>
-                            <Image source={{ uri: /*(parsed.data.url) ? parsed.data.url : */'https://www.elementaryos-fr.org/wp-content/uploads/2019/08/logo-reddit.png' }} />
                             <Text style={{ marginLeft: 16, marginTop: 10, fontSize: 16 }}>{parsed.data.selftext}</Text>
+                            {picOrVid()}
                         </View>
                     </Card.Body>
                     <Card.Footer
